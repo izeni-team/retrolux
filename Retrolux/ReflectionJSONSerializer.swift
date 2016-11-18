603a494e792f997940c6258288c2f286302f111c
Bryan Henderson <cbh2000@gmail.com>
2016-11-17 22:04:51 -0700

    Got ReflectionJSONSerializer to work.

--- Retrolux/ReflectionJSONSerializer.swift
+++ Retrolux/ReflectionJSONSerializer.swift
@@ -33,47 +33,61 @@ class ReflectionJSONSerializer: Serializer {
     
     fileprivate let jsonReadingOptions = JSONSerialization.ReadingOptions.mutableContainers
     
+    fileprivate func convert(from dictionary: [String: Any], to type: Reflectable.Type) throws -> Reflectable {
+        let instance = type.init()
+        let properties = try Reflector().reflect(instance)
+        for property in properties {
+            let rawValue = dictionary[property.mappedTo]
+            try instance.set(value: rawValue, for: property)
+        }
+        return instance
+    }
+    
+    fileprivate func convertToDictionary(_ instance: Reflectable) throws -> [String: Any] {
+        var dictionary: [String: Any] = [:]
+        let properties = try Reflector().reflect(instance)
+        for property in properties {
+            dictionary[property.mappedTo] = try instance.value(for: property)
+        }
+        return dictionary
+    }
+    
     func makeValue<T>(from clientResponse: ClientResponse, type: T.Type) throws -> T {
         guard let data = clientResponse.data else {
             throw ReflectionJSONSerializerError.noData
         }
         
         if let reflectable = T.self as? Reflectable.Type {
-            return () as! T
+            guard let dictionary = (try? JSONSerialization.jsonObject(with: data, options: [])) as? [String: Any] else {
+                throw ReflectionJSONSerializerError.invalidJSON
+            }
+            return try convert(from: dictionary, to: reflectable) as! T
         } else if let array = T.self as? GetTypeFromArray.Type {
             guard let type = array.getReflectableType() else {
                 throw ReflectionJSONSerializerError.unsupportedType(type: T.self)
             }
-            let results = try Reflector().convert(fromJSONArrayData: data, to: Person.self)
-            return results as! T
+            guard let array = (try? JSONSerialization.jsonObject(with: data, options: [])) as? [[String: Any]] else {
+                throw ReflectionJSONSerializerError.invalidJSON
+            }
+            return try array.map { try convert(from: $0, to: type) } as! T
         } else {
             throw ReflectionJSONSerializerError.unsupportedType(type: T.self)
         }
     }
     
     func apply<T>(value input: T, to request: inout URLRequest) throws {
-//        let values = normalizeValues(valueOrValues: input)
-//        
-//        var results: [[String: Any]] = []
-//        
-//        for object in values {
-//            var json: [String: Any] = [:]
-//            let properties = try Reflector().reflect(object)
-//            for property in properties {
-//                json[property.mappedTo] = try object.value(for: property)
-//            }
-//            results.append(json)
-//        }
-//        
-//        let output: Any
-//        if valueOrValues is GetTypeFromArray {
-//            output = valueOrValues
-//        } else {
-//            output = results.first!
-//        }
-//        
-//        let data = try JSONSerialization.data(withJSONObject: output, options: [])
-//        request.httpBody = data
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        if let reflectable = input as? Reflectable {
+            let dictionary = try convertToDictionary(reflectable)
+            let data = try JSONSerialization.data(withJSONObject: dictionary, options: [])
+            request.httpBody = data
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        } else if let array = input as? [Reflectable] {
+            let dictionaries = try array.map { try convertToDictionary($0) }
+            let data = try JSONSerialization.data(withJSONObject: dictionaries, options: [])
+            request.httpBody = data
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        } else {
+            throw ReflectionJSONSerializerError.unsupportedType(type: T.self)
+        }
     }
 }
